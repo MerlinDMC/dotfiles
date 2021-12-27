aa12ec8672ccfe8e64b575c2774339c1fec51201
Daniel Malon <daniel.malon@me.com>
2021-12-27 10:59:56 +0000

    fix(install): install 1password-cli before running chezmoi

--- install.sh
+++ install.sh
@@ -3,9 +3,6 @@
 GITHUB_USER=$1
 ONEPW_EMAIL=$2
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 ## Homebrew
 HOMEBREW_BIN_DIR="/usr/local/bin"
 
@@ -13,6 +10,18 @@ if [[ `uname -m` == 'arm64' ]]; then
   HOMEBREW_BIN_DIR="/opt/homebrew/bin"
 fi
 
-eval $("${HOMEBREW_BIN_DIR}/op" signin my $ONEPW_EMAIL)
+# Install Homebrew
+if [[ ! -e "$HOMEBREW_BIN_DIR/brew" ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+export PATH=$PATH:$HOMEBREW_BIN_DIR
+
+brew bundle --file=- << EOF
+brew "mas"
+cask "1password-cli"
+EOF
+
+eval "$(op signin my $ONEPW_EMAIL)"
 
 env BINDIR=$HOME/.local/bin sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply $GITHUB_USER
